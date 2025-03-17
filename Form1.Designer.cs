01e4f95beb169cfffd22148bae4fc54b8695a3ca
stud <stud@R408-08>
2025-03-17 14:42:06 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBoxSin = new System.Windows.Forms.CheckBox();
+            this.checkBoxCos = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.k = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.phase = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.xmax = new System.Windows.Forms.TextBox();
+            this.xmin = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(80, 21);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.IsXValueIndexed = true;
+            series3.Legend = "Legend1";
+            series3.Name = "Sin(x)";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.LabelBorderWidth = 4;
+            series4.Legend = "Legend1";
+            series4.Name = "Cos(x)";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(621, 256);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(244, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить график функции";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // checkBoxSin
+            // 
+            this.checkBoxSin.AutoSize = true;
+            this.checkBoxSin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxSin.Location = new System.Drawing.Point(42, 360);
+            this.checkBoxSin.Name = "checkBoxSin";
+            this.checkBoxSin.Size = new System.Drawing.Size(75, 28);
+            this.checkBoxSin.TabIndex = 2;
+            this.checkBoxSin.Text = "sin(x)";
+            this.checkBoxSin.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCos
+            // 
+            this.checkBoxCos.AutoSize = true;
+            this.checkBoxCos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxCos.Location = new System.Drawing.Point(42, 390);
+            this.checkBoxCos.Name = "checkBoxCos";
+            this.checkBoxCos.Size = new System.Drawing.Size(81, 28);
+            this.checkBoxCos.TabIndex = 3;
+            this.checkBoxCos.Text = "cos(x)";
+            this.checkBoxCos.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(122, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "y = Sin (";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // k
+            // 
+            this.k.Location = new System.Drawing.Point(196, 318);
+            this.k.Name = "k";
+            this.k.Size = new System.Drawing.Size(22, 20);
+            this.k.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(224, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = " x +";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(301, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = ")";
+            // 
+            // phase
+            // 
+            this.phase.Location = new System.Drawing.Point(273, 318);
+            this.phase.Name = "phase";
+            this.phase.Size = new System.Drawing.Size(22, 20);
+            this.phase.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(608, 361);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "x min =";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(608, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "x max =";
+            // 
+            // xmax
+            // 
+            this.xmax.Location = new System.Drawing.Point(686, 398);
+            this.xmax.Name = "xmax";
+            this.xmax.Size = new System.Drawing.Size(61, 20);
+            this.xmax.TabIndex = 12;
+            this.xmax.Text = " ";
+            // 
+            // xmin
+            // 
+            this.xmin.Location = new System.Drawing.Point(686, 361);
+            this.xmin.Name = "xmin";
+            this.xmin.Size = new System.Drawing.Size(61, 20);
+            this.xmin.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.xmin);
+            this.Controls.Add(this.xmax);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.phase);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.k);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxCos);
+            this.Controls.Add(this.checkBoxSin);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBoxSin;
+        private System.Windows.Forms.CheckBox checkBoxCos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox k;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox phase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox xmax;
+        private System.Windows.Forms.TextBox xmin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Инициализация значений по умолчанию
+            xmin.Text = "-10"; // x min
+            xmax.Text = "10";  // x max
+            k.Text = "0";    // sin(x) phase shift
+            phase.Text = "0";    // cos(x) phase shift
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Вызываем метод для построения графика
+            BuildChart();
+        }
+
+        private void BuildChart()
+        {
+            // Очистка графика перед построением
+            chart1.Series["Sin(x)"].Points.Clear();
+            chart1.Series["Cos(x)"].Points.Clear();
+
+            // Получение значений из текстовых полей
+            if (!double.TryParse(xmin.Text, out double xMin) ||
+                !double.TryParse(xmax.Text, out double xMax) ||
+                !double.TryParse(k.Text, out double koef) ||
+                !double.TryParse(phase.Text, out double cosPhaseShift)) ||
+
+                !double.TryParse(k.Text, out double sinPhaseShift) ||
+                !double.TryParse(phase.Text, out double cosPhaseShift))
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+                return;
+            }
+
+            double h = 0.1; // Шаг
+
+            // Построение графика синуса
+            if (checkBoxSin.Checked)
+            {
+                for (double x = xMin; x <= xMax; x += h)
+                {
+                    double y = Math.Sin(x + sinPhaseShift);
+                    chart1.Series["Sin(x)"].Points.AddXY(x, y);
+                }
+            }
+
+            // Построение графика косинуса
+            if (checkBoxCos.Checked)
+            {
+                for (double x = xMin; x <= xMax; x += h)
+                {
+                    double y = Math.Cos(x + cosPhaseShift);
+                    chart1.Series["Cos(x)"].Points.AddXY(x, y);
+                }
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для chart1 (можно оставить пустым или добавить функционал)
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для label1 (можно оставить пустым или добавить функционал)
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для label5 (можно оставить пустым или добавить функционал)
+        }
+    }
+}
